a24d989b579668c1887636d4ec40873617ab5579
Brad Cleaver <bcleaver@microsoft.com>
2020-08-19 10:52:27 -0700

    InstanceAnnotation example

---
+++ OData/InstanceAnnotations/SimpleController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNet.OData;
+using Microsoft.AspNet.OData.Routing;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ODataAnnotations
+{
+    public class SimpleController : ODataController
+    {
+        [HttpGet]
+        [ODataRoute("/Data")]
+        [EnableQuery]
+        public IActionResult Get()
+        {
+            this.Request.Headers.Add("Prefer", "odata.include-annotations=*");
+
+            return this.Ok(
+                new Simple[]
+                {
+                    new Simple
+                    {
+                        Annotations =
+                        {
+                            { "simple.objectannotation", "objectannotation1" },
+                            { "Name@simple.propertyannotation", "propertyannotation1" }
+                        },
+                        Id = "1",
+                        Name = "One",
+                    },
+                    new Simple
+                    {
+                        Annotations =
+                        {
+                            { "simple.objectannotation", "objectannotation2" },
+                            { "Name@simple.propertyannotation", "propertyannotation2" }
+                        },
+                        Id = "2",
+                        Name = "Two",
+                    },
+                    new Simple
+                    {
+                        Annotations =
+                        {
+                            { "simple.objectannotation", "objectannotation2" },
+                            { "Name@simple.propertyannotation", "propertyannotation2" }
+                        },
+                        Id = "3",
+                        Name = "Three",
+                    },
+                });
+        }
+    }
+}
---
+++ OData/InstanceAnnotations/SimpleResourceSerializer.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNet.OData;
+using Microsoft.AspNet.OData.Formatter.Serialization;
+using Microsoft.OData;
+
+namespace ODataAnnotations
+{
+    public class SimpleResourceSerializer : ODataResourceSerializer
+    {
+        public SimpleResourceSerializer(ODataSerializerProvider serializerProvider) : base(serializerProvider)
+        {
+        }
+
+        public override ODataResource CreateResource(SelectExpandNode selectExpandNode, ResourceContext resourceContext)
+        {
+            ODataResource resource = base.CreateResource(selectExpandNode, resourceContext);
+
+            if (resourceContext.ResourceInstance is IAnnotatable annotatable)
+            {
+                foreach (var annotation in annotatable.Annotations)
+                {
+                    int atIndex = annotation.Key.IndexOf('@');
+                    if (atIndex <= 0)
+                    {
+                        resource.InstanceAnnotations.Add(
+                            new ODataInstanceAnnotation(
+                                annotation.Key.Substring(atIndex + 1),
+                                new ODataPrimitiveValue(annotation.Value)));
+                    }
+                }
+            }
+
+            return resource;
+        }
+
+        public override void AppendDynamicProperties(ODataResource resource, SelectExpandNode selectExpandNode, ResourceContext resourceContext)
+        {
+            base.AppendDynamicProperties(resource, selectExpandNode, resourceContext);
+
+            if (resourceContext.ResourceInstance is IAnnotatable annotatable)
+            {
+                foreach (var annotation in annotatable.Annotations)
+                {
+                    int atIndex = annotation.Key.IndexOf('@');
+                    if (atIndex > 0)
+                    {
+                        string propertyName = annotation.Key.Substring(0, atIndex);
+                        foreach (var property in resource.Properties)
+                        {
+                            if (property.Name == propertyName)
+                            {
+                                property.InstanceAnnotations.Add(
+                                    new ODataInstanceAnnotation(
+                                        annotation.Key.Substring(atIndex + 1),
+                                        new ODataPrimitiveValue(annotation.Value)));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
