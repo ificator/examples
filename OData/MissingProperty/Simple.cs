855c06b93b162418bcb63f41053db17fcf098834
ificator <ificator@outlook.com>
2020-10-06 12:56:49 -0700

    Create issue repro

---
+++ OData/MissingProperty/Simple.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+
+namespace ODataAnnotations
+{
+    public class Simple
+    {
+        [DataMember]
+        public Complex Complex { get; set; }
+
+        [Key]
+        public string Id { get; set; }
+
+        [DataMember]
+        public string Name { get; set; }
+    }
+
+    public class Complex
+    {
+        public string Value { get; set; }
+    }
+}
---
+++ OData/MissingProperty/SimpleController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNet.OData;
+using Microsoft.AspNet.OData.Routing;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ODataAnnotations
+{
+    public class SimpleController : ODataController
+    {
+        [HttpGet]
+        [ODataRoute("/Data")]
+        [EnableQuery]
+        public IActionResult Get()
+        {
+            bool excludeComplex = this.Request.Query["repro"] == "1";
+
+            return this.Ok(
+                new Simple[]
+                {
+                    new Simple
+                    {
+                        Complex = excludeComplex ? null : new Complex { Value = "c1" },
+                        Id = "1",
+                        Name = "One",
+                    },
+                    new Simple
+                    {
+                        Complex = excludeComplex ? null : new Complex { Value = "c2" },
+                        Id = "2",
+                        Name = "Two",
+                    },
+                    new Simple
+                    {
+                        Complex = excludeComplex ? null : new Complex { Value = "c3" },
+                        Id = "3",
+                        Name = "Three",
+                    },
+                });
+        }
+    }
+}
